1b3da84811fccded2f7d1f01d5aeab86e0448a3c
JordanZ99 <jordyz2005@gmail.com>
2026-03-02 18:30:23 -0500

    Add product resource routes

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,9 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+// Debajo de la línea 7 agrega:
+Route::resource('products', 'ProductController');
+
 use App\Http\Controllers\ProductController;
 
 Route::resource('products', ProductController::class);
